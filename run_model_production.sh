4f4c594961807fd4a98bc6d2274a08bcbdef3afb
Lavino Menezes <lprogramacao@protonmail.com>
2023-01-30 08:26:09 -0300

    run-prod

--- run_model_production.sh
+++ run_model_production.sh
@@ -3,8 +3,8 @@
 data=$(date +"%d_%m_%Y_%H_%M_%S")
 
 path='/home/ubuntu/insiders_loyalty_program'
-#path_to_bibli='/home/ubuntu/.pyenv/versions/insider_clustering/bin'
-path_to_bibli= '/home/ubuntu/.pyenv/shims'
+path_to_bibli='/home/ubuntu/.pyenv/versions/insider_clustering/bin'
+#path_to_bibli= '/home/ubuntu/.pyenv/shims'
 $path_to_bibli/papermill $path/src/models/insider_clustering_v10_deploy_light_production.ipynb $path/report/insider_clustering_v09_deploy_light_$data.ipynb
 
 
